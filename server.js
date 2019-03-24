a2b175c04ce75ab096bb07584b7c18ce51454d27
Jacob Spade <spade.jacob@Yahoo.com>
2019-03-24 13:14:49 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const onRequest = (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello World');
+  response.end();
+}
+
+http.createServer(onRequest).listen(5000);
+
